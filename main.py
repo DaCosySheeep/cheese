24add9d53e160664dad7a974bbe941420d2570e2
DaCosySheeep <tjgorgeous@hotmail.com>
2023-11-28 20:46:12 +0000

    Find files when compiled to .exe

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from tkinter import *
 from random import randint
 from PIL import Image, ImageTk
-import pygame
+import pygame, os, sys
 global imageshown
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
 pygame.mixer.init()
 root=Tk()
 root.title("Load Cheese")
@@ -14,13 +25,13 @@ def show_cheese():
     print(r)
     if imageshown==True:
         root.img.destroy()
-    render=ImageTk.PhotoImage(Image.open(f"images/cheese{r}.png"))
+    render=ImageTk.PhotoImage(Image.open(resource_path(f"images/cheese{r}.png")))
     root.img=Label(root, image=render)
     root.img.image=render
     #img.place(x=10,y =10)
     root.img.pack()
     imageshown=True
-    pygame.mixer.music.load(f"sounds/cheese{r}.mp3")
+    pygame.mixer.music.load(resource_path(f"sounds/cheese{r}.mp3"))
     pygame.mixer.music.play()
     
 button=Button(root, text="We like cheese", command=show_cheese)
