cc3514e63b6f3b51d9bd66c3bdadc96cbc1f261f
DaCosySheeep <tjgorgeous@hotmail.com>
2023-12-01 14:27:52 +0000

    Cheese 2.1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from random import randint
 from PIL import Image, ImageTk
-import pygame, os, sys
+import pygame
 global imageshown
 
 def resource_path(relative_path):
@@ -16,25 +16,41 @@ def resource_path(relative_path):
 
 pygame.mixer.init()
 root=Tk()
-#root.state('zoomed')
-root.attributes('-zoomed', True)
+try:
+    root.state('zoomed')
+except:
+    root.attributes('-zoomed', True)
 root.title("Load Cheese")
 root.geometry("40x60")
 
 imageshown=False
 def show_cheese():
     global imageshown
-    r=randint(1,10)
-    print(r)
-    if imageshown==True:
-        root.img.destroy()
-    render=ImageTk.PhotoImage(Image.open(resource_path(f"images/cheese{r}.png")))
-    root.img=Label(root, image=render)
-    root.img.image=render
-    #img.place(x=10,y =10)
-    root.img.pack()
-    imageshown=True
-    pygame.mixer.music.load(resource_path(f"sounds/cheese{r}.mp3"))
+    r=randint(1,11)
+    if r == 11:
+        r=randint(1,11)
+    if r==11:
+        print("Imposter")
+        if imageshown==True:
+            root.img.destroy()
+        render=ImageTk.PhotoImage(Image.open(resource_path(f"images/potato.png")))
+        root.img=Label(root, image=render)
+        root.img.image=render
+        #img.place(x=10,y =10)
+        root.img.pack()
+        imageshown=True
+        pygame.mixer.music.load(resource_path(f"sounds/imposter.mp3"))
+    else:
+        print(r)
+        if imageshown==True:
+            root.img.destroy()
+        render=ImageTk.PhotoImage(Image.open(resource_path(f"images/cheese{r}.png")))
+        root.img=Label(root, image=render)
+        root.img.image=render
+        #img.place(x=10,y =10)
+        root.img.pack()
+        imageshown=True
+        pygame.mixer.music.load(resource_path(f"sounds/cheese{r}.mp3"))
     pygame.mixer.music.play()
     
 button=Button(root, text="We like cheese", command=show_cheese)
