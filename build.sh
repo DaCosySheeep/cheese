87840617d95017e06e6e814712f57dd4ee15ff76
tjgor <tjgor@Ubuntu.myguest.virtualbox.org>
2024-05-25 16:33:15 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-pyinstaller --noconfirm --onefile --windowed --icon "images/cheese.ico" --name "Cheese-2.2" --add-data="sounds:sounds/" --add-data="images:images/"  "main.py"
+pyinstaller --noconfirm --onefile --windowed --icon "images/cheese.ico" --name "Cheese-2.2-Ubuntu" --add-data="sounds:sounds/" --add-data="images:images/"  "main.py" --hidden-import="PIL._tkinter_finder"
+./dist/Cheese-2.2-Ubuntu
